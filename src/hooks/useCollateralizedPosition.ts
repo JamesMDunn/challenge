a9b79c61b13a20ad5e11b546fc17837f85f8e0c9
James Dunn <jamesallstar21@gmail.com>
2021-11-02 12:49:53 -0400

    cleaned up the hook and removed logs

--- src/hooks/useCollateralizedPosition.ts
+++ src/hooks/useCollateralizedPosition.ts
@@ -2,29 +2,39 @@ import { CollateralizedPosition } from '../types/CollateralizedPosition';
 import {PublicKey} from "@solana/web3.js";
 import {useCallback, useEffect, useState} from "react";
 import {getObligation} from "../larix-api/provider/obligationProvider";
-import {LAMPORTS_PER_SOL} from "../constants";
-import BN from "bn.js";
 import {getLendingReserve} from "../larix-api/provider/lendingReserveProvider";
-import {Detail, Reserve} from "../larix-api/models";
+import {Detail, ObligationCollateral, ObligationLiquidity, Reserve} from "../larix-api/models";
 import {getInterest} from "../larix-api/utils/rateModel";
 import {getUtilizationRate} from "../larix-api/utils/calculateAllMine";
 import { eX } from '../larix-api/utils/helpers';
+import BigNumber from 'bignumber.js';
 
 
 export const useCollateralizedPosition = (pubkey: PublicKey | null) => {
     const [collateralizedPositions, setCollateralizedPositions] = useState([] as CollateralizedPosition[]);
 
+    const getInterestAndUtilization = (reserve: Detail<Reserve>) => {
+        const {compoundBorrowInterestArray,compoundSupplyInterestArray} = getInterest(reserve);
+        const utilizationRate = getUtilizationRate(reserve.info)
+        let borrowApy = 0;
+        let depositApy = 0;
+        compoundBorrowInterestArray.forEach(b => {
+            let round = Math.ceil((utilizationRate.toNumber() * 100)) / 100
+            if (b[0] === round) borrowApy = b[1]
+        });
+        compoundSupplyInterestArray.forEach(b => {
+            let round = Math.ceil((utilizationRate.toNumber() * 100)) / 100
+            if (b[0] === round) depositApy = b[1]
+        });
+        return {utilizationRate, borrowApy, depositApy}
+    }
+
+
     const getPositions = useCallback(async () => {
-        console.log("got here")
         if (pubkey) {
             const larixObligations = await getObligation(pubkey);
             const lendingReserves = await getLendingReserve();
-            console.log({larixObligations, lendingReserves})
             larixObligations.forEach(obligation => {
-            console.log({obligation})
-            // console.log(obligation.pubkey.toString())
-            // console.log(obligation.info.deposits[0].depositedAmount.toString())
-            // console.log(obligation.info.borrows[0].borrowedAmountWads.toString())
                 obligation.info.deposits.forEach(deposit => {
                     let reserve: Detail<Reserve> = {} as Detail<Reserve>;
                     lendingReserves.forEach(res => {
@@ -32,27 +42,15 @@ export const useCollateralizedPosition = (pubkey: PublicKey | null) => {
                            reserve = res;
                         }
                     })
-                    const {compoundBorrowInterestArray,compoundSupplyInterestArray} = getInterest(reserve);
-                    const utilizationRate = getUtilizationRate(reserve.info)
-                    let borrowApy = 0;
-                    let depositApy = 0;
-                    compoundBorrowInterestArray.forEach(b => {
-                        let round = Math.ceil((utilizationRate.toNumber() * 100)) / 100
-                        console.log(b[0], round)
-                        if (b[0] === round) borrowApy = b[1]
-                    });
-                    compoundSupplyInterestArray.forEach(b => {
-                        let round = Math.ceil((utilizationRate.toNumber() * 100)) / 100
-                        console.log(b[0], round)
-                        if (b[0] === round) depositApy = b[1]
-                    });
+                    const {borrowApy, depositApy} = getInterestAndUtilization(reserve);
+                    console.log({reserve})
                     const cp: CollateralizedPosition = {
-                        platform: "Larix", //TODO not sure if this is hardcoded?
-                        amount: deposit.depositedAmount.toNumber() / 1000000000,
+                        platform: "Larix", 
+                        amount: eX(deposit.depositedAmount.toNumber(), -reserve.info.liquidity.mintDecimals).toNumber(),
                         mint: reserve?.info.collateral.mintPubkey.toString(),
-                        borrowApy, //TODO
-                        depositApy, //TODO
-                        supplied: true //TODO
+                        borrowApy, 
+                        depositApy, 
+                        supplied: true
                     }
                     setCollateralizedPositions((prev) => [...prev, cp])
                 })
@@ -64,24 +62,8 @@ export const useCollateralizedPosition = (pubkey: PublicKey | null) => {
                             borrowReserve = res;
                         }
                     })
-                    console.log({borrowReserve, borrow})
-                    let borrowApy = 0;
-                    let depositApy = 0;
-                    const utilizationRate = getUtilizationRate(borrowReserve.info)
-
+                    const {borrowApy, depositApy} = getInterestAndUtilization(borrowReserve);
                     let amount = eX(borrow.borrowedAmountWads.toString(),-18)
-                    const {compoundBorrowInterestArray,compoundSupplyInterestArray} = getInterest(borrowReserve);
-                    console.log({compoundBorrowInterestArray,compoundSupplyInterestArray})
-                    compoundBorrowInterestArray.forEach(b => {
-                        let round = Math.ceil((utilizationRate.toNumber() * 100)) / 100
-                        console.log(b[0], round)
-                        if (b[0] === round) borrowApy = b[1]
-                    });
-                    compoundSupplyInterestArray.forEach(b => {
-                        let round = Math.ceil((utilizationRate.toNumber() * 100)) / 100
-                        console.log(b[0], round)
-                        if (b[0] === round) depositApy = b[1]
-                    });
                     const cp: CollateralizedPosition = {
                         platform: "Larix", 
                         amount: amount.shiftedBy(-borrowReserve.info.liquidity.mintDecimals).toNumber(),
@@ -90,7 +72,6 @@ export const useCollateralizedPosition = (pubkey: PublicKey | null) => {
                         depositApy, 
                         supplied: false
                     }
-                    console.log(borrowReserve.info.liquidity.mintDecimals)
                     setCollateralizedPositions((prev) => [...prev, cp])
                 })
             })
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,87 +1,12 @@
-import { WalletMultiButton } from "@solana/wallet-adapter-ant-design";
-import {Connection} from "@solana/web3.js";
-import { Button, Col, Row, Table } from "antd";
-import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
-import { TokenIcon } from "../../components/TokenIcon";
-import { useConnectionConfig } from "../../contexts/connection";
-import { useMarkets } from "../../contexts/market";
-import {useUserAccounts, useUserBalance, useUserTotalBalance} from "../../hooks";
-import { WRAPPED_SOL_MINT } from "../../utils/ids";
-import { formatUSD } from "../../utils/utils";
-import {getTokenAccounts} from "../../larix-api/provider/tokenAccountsProvider";
+import { Col, Row, Table } from "antd";
+import React from "react";
 import {useWallet} from "@solana/wallet-adapter-react";
-import {getLendingReserve} from "../../larix-api/provider/lendingReserveProvider";
-import {getLendingMarket} from "../../larix-api/provider/lendingMarketProvider";
-import {getObligation, getObligations} from "../../larix-api/provider/obligationProvider";
-import {getConnection} from "../../larix-api/context/connection";
-import {TOKEN_PROGRAM_ID, AccountLayout} from "@solana/spl-token";
-import * as anchor from '@project-serum/anchor'
-import {Detail, LendingMarketParser, Reserve} from "../../larix-api/models";
-import {LENDING_ID} from "../../larix-api/utils/ids";
 import {useCollateralizedPosition} from "../../hooks/useCollateralizedPosition";
-import {getMining} from "../../larix-api/provider/miningProvider";
 
 export const HomeView = () => {
-  const { marketEmitter, midPriceInUSD } = useMarkets();
-  const { endpoint } = useConnectionConfig();
-  const SRM_ADDRESS = "SRMuApVNdxXokk5GT7XD5cUUgXMBCoAz2LHeuAoKWRt";
-  const SRM = useUserBalance(SRM_ADDRESS);
-  const SOL = useUserBalance(WRAPPED_SOL_MINT);
-  const { balanceInUSD: totalBalanceInUSD } = useUserTotalBalance();
   const { publicKey } = useWallet();
   const { collateralizedPositions } = useCollateralizedPosition(publicKey);
 
-  const test = async () => {
-    if (publicKey) {
-      const userTokenAccounts = await getTokenAccounts(publicKey);
-      // console.log("userTokenAccounts", userTokenAccounts);
-      // let test = userTokenAccounts.get("ArWoXQYvvERzuoeGTpyyvf7pNNVEPB3x7kQg5W6zQdaB")
-      // console.log(test)
-      // const larixReserve = await getLendingReserve();
-      // const larixLendingMarket = await getLendingMarket();
-      // const larixMining = await getMining(publicKey);
-      // console.log('larixMining', larixMining)
-      // console.log("larixLending", larixLendingMarket);
-      // console.log("larixReserve", larixReserve)
-      // const reserves: Detail<Reserve>[] = []
-      // larixReserve.forEach(acc => {
-      //   if (userTokenAccounts.has(acc.info.liquidity.mintPubkey.toString())){
-      //     const tokenAcc = userTokenAccounts.get(acc.info.liquidity.mintPubkey.toString());
-      //     if (tokenAcc) {
-      //       console.log("user t acc", tokenAcc[0].pubkey.toString(), tokenAcc)
-      //       console.log(tokenAcc[0].info.amount.toString())
-      //     }
-      //     console.log("got here", acc.info.liquidity.mintPubkey.toString(), acc)
-
-
-          // console.log("mintKey liquid", acc.info.liquidity.mintPubkey.toString())
-          // console.log("supplyKey liquid",acc.info.liquidity.supplyPubkey.toString())
-          //
-          // console.log("mintKey collat", acc.info.collateral.mintPubkey.toString())
-          // console.log("supplyKey collat",acc.info.collateral.supplyPubkey.toString())
-          // console.log("availableAmount", acc.info.liquidity.availableAmount.toString())
-          // reserves.push(acc)
-        // }
-      // });
-    }
-  }
-
-  useEffect(() => {
-    test()
-  }, [publicKey])
-
-
-  const dataSource = [
-    {
-      platform: 'test',
-      mint: 'test',
-      supplied: false,
-      amount: 20,
-      borrowApy: 20,
-      depositApy: 20,
-    },
-  ];
 
   const columns = [
     {
